bdab9f222b2fd07a8db7f880f62a84bd8debf225
Dustin M <justanothernerdhere@outlook.com>
2023-01-02 21:45:10 +0100

    fix: not running with correct args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use std::env;
+use std::{env, process::Command};
 use fork::{daemon, Fork};
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
+    // dbg!("Running: {}", &args[1]);
+    // dbg!("With args: {:?}", &args[2..]);
+
     if let Ok(Fork::Child) = daemon(false, false) {
-        let _ = exec::execvp(&args[1], &args[2..]);
+        let _ = Command::new(&args[1])
+        .args(&args[2..])
+        .output()
+        .expect("Error running command");
     }
 
 }
